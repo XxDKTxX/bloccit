d8a2d83c9fcbb1d9b730fc9cb11cddf58f394b39
Devin Tassie <devintassie@gmail.com>
2018-02-09 00:42:20 +0000

    test assignment complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/faq'
+
   get 'welcome/index'
 
   get 'welcome/about'
   
   get 'welcome/contact'
+  
+  get 'welcome/faq'
 
   root 'welcome#index'
   
