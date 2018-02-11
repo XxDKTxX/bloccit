746dae5603cbf2a1fa5f4301da56d4b577df8121
Devin Tassie <devintassie@gmail.com>
2018-02-11 17:42:24 +0000

    save point, working on assignment. how do i link custom comment to custom post?

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,15 @@ require 'random_data'
  # #4
      post: posts.sample,
      body: RandomData.random_paragraph
-   )
+   ) 
  end
  
+ puts "creating custom post"
+Post.find_or_create_by!(title: "custom post", body: "custom body")
+ puts "creating custom comment"
+Comment.find_or_create_by!(post: "custom post", body: "custom comment")
+  
+  
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
