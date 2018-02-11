39a448c17a226d5682ee4f811555a70509c8765a
Devin Tassie <devintassie@gmail.com>
2018-02-11 18:43:07 +0000

    crud assignment complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_endex do |post, index|
+      index += 1
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
